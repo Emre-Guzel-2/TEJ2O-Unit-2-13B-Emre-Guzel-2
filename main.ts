9477aa039d336182104d2631a36e8f80d8b5fa24
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-29 15:22:24 +0000

    changed the names and started to set up the neopixels

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Oct 29 2024
+ * This program counts doen from 0 to 4
 */
 
-basic.showString('Hello, World!')
+// Setting the veribles 
+let Leds: neopixel.Strip = null
+let loopCounter = 0
+
+// Setting the screen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+// Setting the neopixel
+Leds= neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+// Setting the lights 
+Leds.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+Leds.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+Leds.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+Leds.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+Leds.show()
+
+input.onButtonPressed(Button.A,function(){
+    basic.clearScreen()
+    loopCounter = 0 
+
+})
